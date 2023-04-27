7652ea6b4f1c3459b97830aacc46ae7ebfaa51f4
vansw <vansw@naver.com>
2023-04-27 23:48:56 +0900

    setting log

--- holiday/settings/production.py
+++ holiday/settings/production.py
@@ -3,3 +3,21 @@ from .base import *
 
 ALLOWED_HOSTS = ['3.34.75.143']
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_debug.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
--- holiday/wsgi.py
+++ holiday/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'holiday.settings')
+settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'holiday.settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
