bc338cbd7b8fae03b79a30d282e40cac29d7e58b
vansw <vansw@naver.com>
2023-05-15 07:40:24 +0900

    user admin update

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,15 +1,35 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import CustomUser
 from .models import PrivateMessage
 
-class CustomUserAdmin(UserAdmin):
-    # You can customize the fields you want to display in the admin site
-    # by modifying the list_display attribute
+class CustomUserAdmin(BaseUserAdmin):
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin.
     list_display = ('username', 'is_staff', 'is_active', 'is_superuser')
+    list_filter = ('is_staff', 'is_superuser', 'is_active',)
 
-    # You can also customize the fields to be used in the add user form
-    # by modifying the add_fieldsets attribute
+    # define the fields for the add and change forms
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+    )
+
+    # fields to be used in user creation form
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2')}
+        ),
+    )
+
+    search_fields = ('username',)
+    ordering = ('username',)
+    filter_horizontal = ()
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
+
 
 
 class PrivateMessageAdmin(admin.ModelAdmin):
@@ -20,4 +40,3 @@ class PrivateMessageAdmin(admin.ModelAdmin):
 admin.site.register(PrivateMessage, PrivateMessageAdmin)
 
 
-admin.site.register(CustomUser, CustomUserAdmin)
